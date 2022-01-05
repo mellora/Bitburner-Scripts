8bbc74a08bc8b1ce331f5db1d5953cb8457e5b05
mellora <AlexMellor2014@gmail.com>
2022-01-05 00:15:00 -0600

    MODIFIED: started writing an auto purchase script

--- kill-servers.js
+++ kill-servers.js
@@ -1,13 +1,16 @@
 /** @param {import(".").NS } ns */
 export async function main(ns) {
-  const pServerPrefix = 'pserv-'
+  ns.disableLog("ALL");
+
+  const pServerPrefix = "p-server-";
   for (let i = 0; i < ns.getPurchasedServerLimit(); i++) {
     let pServer = pServerPrefix + i;
-    if(ns.serverExists(pServer)) {
+    if (ns.serverExists(pServer)) {
       ns.killall(pServer);
       await ns.sleep(10);
       ns.deleteServer(pServer);
       await ns.sleep(10);
+      ns.print("Killed server " + pServer);
     }
   }
 }
