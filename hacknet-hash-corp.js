757e7e2b7b0f81e3b47f7b8cfb09849697b46fda
mellora <AlexMellor2014@gmail.com>
2022-04-20 07:32:39 -0500

    ADDED: Created file to automate purchase of corperation funds or research

---
+++ hacknet-hash-corp.js
@@ -0,0 +1,31 @@
+let doc = eval("document");
+/** @param {import(".").NS } ns */
+export const main = async (ns) => {
+  ns.disableLog("ALL");
+
+  while (true) {
+    const corpFunds = {
+      name: "Sell for Corporation Funds",
+      cost: ns.hacknet.hashCost("Sell for Corporation Funds"),
+    };
+    const corpResearch = {
+      name: "Exchange for Corporation Research",
+      cost: ns.hacknet.hashCost("Exchange for Corporation Research"),
+    };
+
+    let hash;
+    if (corpFunds.cost <= corpResearch.cost) {
+      hash = corpFunds;
+    } else {
+      hash = corpResearch;
+    }
+
+    if (ns.hacknet.numHashes() > hash.cost) {
+      ns.hacknet.spendHashes(hash.name);
+      ns.print("Purchased:");
+      ns.print(hash);
+    } else {
+      await ns.sleep(1);
+    }
+  }
+};
