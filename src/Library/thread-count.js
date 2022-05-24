b352231a403b4686a973b40baae676cdc06a75c6
mellora <AlexMellor2014@gmail.com>
2022-05-24 10:05:10 -0500

    MODIFIED: re-added mistakenly removed variable that broke code

--- src/Library/thread-count.js
+++ src/Library/thread-count.js
@@ -7,6 +7,7 @@ const getThreadCount = (ns, hostName, scriptName, memBuffer = 0) => {
   let maxRam = ns.getServerMaxRam(hostName);
   let usedRam = ns.getServerUsedRam(hostName);
   let scriptRam = ns.getScriptRam(scriptName);
+  let count = 0;
 
   count = Math.floor((maxRam - usedRam - memBuffer) / scriptRam);
 
