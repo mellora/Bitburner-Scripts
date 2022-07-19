51b8f40f594dda12e2dd9dae6cc40e61e806cb43
mellora <AlexMellor2014@gmail.com>
2022-07-19 11:48:06 -0500

    MODIFIED: updated scripts

--- src/Library/thread-count.js
+++ src/Library/thread-count.js
@@ -1,9 +1,11 @@
-/** @param {import("./../..").NS } ns */
+/**
+ * Arrow Function to get available number of threads on a server for a givin script
+ * @param {import("./../..").NS } ns
+ * @param {hostName} Name of host to calculate ram availability for.
+ * @param {scriptName} Name of script to calculate number of runnable threads for script size
+ * @param {memBuffer} Optional - Additional memory to leave available for thread calculation
+ */
 const getThreadCount = (ns, hostName, scriptName, memBuffer = 0) => {
-  /**
-   * Arrow Function to get available number of threads
-   * on a server for a givin script
-   */
   let maxRam = ns.getServerMaxRam(hostName);
   let usedRam = ns.getServerUsedRam(hostName);
   let scriptRam = ns.getScriptRam(scriptName);
--- src/run-hack-threaded.js
+++ src/run-hack-threaded.js
@@ -2,12 +2,12 @@ import getThreadCount from "./Library/thread-count.js";
 
 /** @param {import("./..").NS } ns */
 export const main = async (ns) => {
-  ns.nuke("n00dles");
-  ns.run(
+  let threadCount = getThreadCount(
+    ns,
+    "home",
     "early-hack-template.js",
-    getThreadCount(ns, "home", "early-hack-template.js", {
-      bigScript: "script-crime.js",
-    }),
-    "n00dles"
+    ns.getScriptRam("script-crime.js")
   );
+  ns.nuke("n00dles");
+  ns.run("early-hack-template.js", threadCount, "n00dles");
 };
